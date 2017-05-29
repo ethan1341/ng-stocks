1d107d905ed34c09c7bd62732a9022e776bdec75
ethanpidcock <ethan1341@yahoo.com>
2017-05-29 12:33:58 -0400

    Removed Init code needs to be refactored

--- app/scripts/src/app.js
+++ app/scripts/src/app.js
@@ -2,7 +2,7 @@
  * The base file for the app. Library injection and code to run at init should be placed here
  * @type {angular.Module}
  */
-å
+
 var app = angular.module('ngStocks', ['ui.router','chart.js']);
 
 app.run(['$rootScope', '$window', '$state', 'userService',
