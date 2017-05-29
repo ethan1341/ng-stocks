8d8253fe655ec684cb67b085a448f702fd7b45f6
ethanpidcock <ethan1341@yahoo.com>
2017-05-29 11:00:38 -0400

    TODO: Refactor  one stock is posted user only looks up one stock not all of the existing stocks.
    TODO: When a stock is removed, from client, just remove from database and UI. Don't resend the requests.
    
    Child Controller : $scope.$parent.graph = data;
    Parent Controller: $scope.graph = data;

--- server.js
+++ server.js
@@ -11,22 +11,23 @@ var stock = require('./routehandlers/stock').stock;
 var utils = require('./utils/utils').utils;
 
 
-app.listen(4000,function(){
+app.listen(4000, function () {
     console.log(4000);
 });
 
 app.use(express.static(__dirname + '/'));
 app.use(require('cookie-parser')());
-app.use(require('body-parser').json({ extended: true }));
-app.use(require('express-session')({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(require('body-parser').json({extended: true}));
+app.use(require('express-session')({secret: 'keyboard cat', resave: true, saveUninitialized: true}));
 app.use(passport.initialize());
 app.use(passport.session());
 
 var db = mongoose.connection;
 
-var connect = mongoose.connect(process.env.DB_ADDRESS, function(err) {
+var connect = mongoose.connect(process.env.DB_ADDRESS, function (err) {
 });
-db.once('open', function() {});
+db.once('open', function () {
+});
 
 
 //If I put this in a function outside server.js and run the function here does it work because of closures?
@@ -35,72 +36,68 @@ passport.use(new Strategy({
         clientSecret: process.env.FB_CLIENTSECRET,
         callbackURL: 'http://localhost:4000/facebook/return'
     },
-    function(accessToken, refreshToken, profile, cb) {
+    function (accessToken, refreshToken, profile, cb) {
         console.log(profile.display)
-        utils.findDupe(User,'userID',profile.id).then(function(data){
-            if(data == null){
-                utils.createRecord('User',profile).then(
-                    function(data){
+        utils.findDupe(User, 'userID', profile.id).then(function (data) {
+            if (data == null) {
+                utils.createRecord('User', profile).then(
+                    function (data) {
                         console.log('here')
-                        return cb(null,data)
+                        return cb(null, data)
                     },
-                    function(err){
+                    function (err) {
 
                     });
-            }else{
-                return cb(null,data)
+            } else {
+                return cb(null, data)
 
             }
-        },function(err){console.log(err)});
+        }, function (err) {
+            console.log(err)
+        });
 
     }));
 
-passport.serializeUser(function(user, cb) {
+passport.serializeUser(function (user, cb) {
     cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
+passport.deserializeUser(function (obj, cb) {
     cb(null, obj);
 });
 
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     res.sendFile(path.resolve('./app/html/index.html'));
 });
 
 
-app.get('/logout', function(req, res) {
+app.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/#!/register');
 });
 
-app.get('/stocks',stock.getUserStocks);
+app.get('/stocks', user.isAuthenticated, stock.getUserStocks);
 
-app.get('/stockCharts',stock.getUserStockCharts);
+app.get('/stockCharts', user.isAuthenticated, stock.getUserStockCharts);
 
-app.post('/stocks',stock.stockLookUp);
+app.post('/stocks', user.isAuthenticated, stock.stockLookUp);
 
-app.post('/register',function(req,res){
+app.post('/removeStock', user.isAuthenticated, stock.removeStock)
+
+app.post('/register', function (req, res) {
 
 });
 
-app.get('/userInfo',user.checkSession);
+console.log('dr')
+
+app.get('/userInfo', user.isAuthenticated, user.checkSession);
 
 app.get('/facebook',
     passport.authenticate('facebook'));
 
 app.get('/facebook/return',
-    passport.authenticate('facebook', { failureRedirect: '/#!/register' }),
-    function(req, res) {
+    passport.authenticate('facebook', {failureRedirect: '/#!/register'}),
+    function (req, res) {
         res.redirect('/#!/redirect');
     });
-
-
-function isAuthenticated(req, res, next){
-    if (req.user) {
-        next();
-    } else {
-        res.redirect('/');
-    }
-};
-
 
